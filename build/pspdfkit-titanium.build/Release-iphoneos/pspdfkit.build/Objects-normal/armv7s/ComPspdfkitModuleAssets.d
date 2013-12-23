2cf8ee349205cf8d7d9437959732bb7d0b169058
Peter Steinberger <me@petersteinberger.com>
2013-12-23 23:29:04 +0100

    remove build folder

--- build/pspdfkit-titanium.build/Release-iphoneos/pspdfkit.build/Objects-normal/armv7s/ComPspdfkitModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/steipete/Documents/Projects/PSPDFKit-Titanium/Classes/ComPspdfkitModuleAssets.m \
-  /Users/steipete/Documents/Projects/PSPDFKit-Titanium/Classes/ComPspdfkitModuleAssets.h
